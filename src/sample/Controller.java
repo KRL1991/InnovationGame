0b5cccc467738e0b8fb525911b3a1575b544068f
MathiasMadsen <m1999@live.dk>
2021-01-14 10:24:13 +0100

    Started scenebuilder

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button StartGameButton;
+
+    @FXML
+    private Button LoadGameButton;
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,9 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
+
 public class Main extends Application {
+
+    Scene scene2;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -14,6 +21,15 @@ public class Main extends Application {
         primaryStage.setTitle("PropertyInvestor");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+
+    /*
+        Button button2 = new Button("scene2");
+        Label label2 = new Label("Dette er scene 2");
+        button2.setOnAction(e -> primaryStage.setScene(scene2));
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(button2, label2);
+        scene2 = new Scene(root,200,200);
+        */
     }
 
 
